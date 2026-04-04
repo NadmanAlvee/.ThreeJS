488e9ae84d75fbeb27ff08141f3f04fbd5026e9a
NadmanAlvee <nadmanalvee@gmail.com>
2026-04-04 14:33:29 +0600

    transforming to oop

--- chapter_3_3DModels/src/js/index.js
+++ chapter_3_3DModels/src/js/index.js
@@ -1,1 +1,112 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+class World {
+  // constructor
+  constructor() {
+    this.renderer = this.#initRenderer();
+    this.scene = this.#initScene();
+    this.camera = this.#initPerspectiveCamera();
+
+    this.controls = this.#initOrbitControl();
+    this.gltfLoader = this.#initGltfLoader();
+
+    this.#initLights();
+    this.#initObjects();
+
+    this.#initAnimationLoop();
+    this.#initResize();
+  }
+
+  // Render
+  #initRenderer() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
+    document.body.append(renderer.domElement);
+    return renderer;
+  }
+
+  // Scene
+  #initScene() {
+    const scene = new THREE.Scene();
+    return scene;
+  }
+
+  // Camera
+  #initPerspectiveCamera() {
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000,
+    );
+    camera.position.set(0, 5, -10);
+    return camera;
+  }
+
+  // Orbit Control
+  #initOrbitControl() {
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.update();
+    return controls;
+  }
+
+  // Gltf Loader
+  #initGltfLoader() {
+    const gltfLoader = new GLTFLoader();
+    return gltfLoader;
+  }
+
+  // Lights
+  #initLights() {
+    const ambientLight = new THREE.AmbientLight(0x333333);
+    this.scene?.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight("#ffffff", 2);
+    directionalLight.position.set(10, 20, 0);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera.bottom = -12;
+    this.scene?.add(directionalLight);
+  }
+
+  // Initiate Objects
+  #initObjects() {
+    // plane
+    const planeGeometry = new THREE.PlaneGeometry(20, 20);
+    const planeMaterial = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      side: THREE.DoubleSide,
+    });
+    const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+    planeMesh.rotation.x = -0.5 * Math.PI;
+    planeMesh.receiveShadow = true;
+    this.scene.add(planeMesh);
+
+    this.gltfLoader.load("../models/LP_Person.glb", (gltf) => {
+      this.scene.add(gltf.scene);
+    });
+  }
+
+  // Animate
+  #initAnimationLoop() {
+    this.renderer.setAnimationLoop(() => {
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  // Handle Screen Resize
+  #initResize() {
+    window.addEventListener("resize", () => {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+    });
+  }
+}
+
+const world = new World();
