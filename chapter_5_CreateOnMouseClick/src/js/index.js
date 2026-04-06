b4949dcc0c04a0521bece4c27613aa5912a6c357
NadmanAlvee <nadmanalvee@gmail.com>
2026-04-06 23:40:42 +0600

    Implement object creation on mouse click and adjust ground mesh properties

--- chapter_5_CreateOnMouseClick/src/js/index.js
+++ chapter_5_CreateOnMouseClick/src/js/index.js
@@ -16,6 +16,8 @@ class World {
     this.#initBackground();
     this.#initObjects();
     this.groundMesh = null;
+    this.axesHelper = null;
+    this.#makeObjectOnClick();
 
     this.#initAnimationLoop();
     this.#initResize();
@@ -46,7 +48,7 @@ class World {
       0.1,
       1000,
     );
-    camera.position.set(0, 15, 30);
+    camera.position.set(0, 0, 30);
     return camera;
   }
 
@@ -85,8 +87,8 @@ class World {
   // Initiate Objects
   #initObjects() {
     // ground
-    const groundGeometry = new THREE.PlaneGeometry(40, 40);
-    const groundMaterial = new THREE.MeshStandardMaterial({
+    const groundGeometry = new THREE.PlaneGeometry(40, 40, 5, 5);
+    const groundMaterial = new THREE.MeshBasicMaterial({
       color: 0x0000ff,
       side: THREE.DoubleSide,
       wireframe: true,
@@ -94,7 +96,49 @@ class World {
     const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
     this.groundMesh = groundMesh;
     groundMesh.receiveShadow = true;
-    this.scene.add(this.groundMesh);
+    this.groundMesh.rotateX(Math.PI * 0.5);
+    // this.scene.add(this.groundMesh);
+
+    // Axes helper
+    this.axesHelper = new THREE.AxesHelper(10);
+    this.scene.add(this.axesHelper);
+  }
+
+  // Make Objects on Click
+  #makeObjectOnClick() {
+    // coord of the mouse posi
+    const mouse = new THREE.Vector2();
+    // coord of the mouse click
+    const intersectionPoint = new THREE.Vector3();
+    // direction of the plain
+    const planeNormal = new THREE.Vector3();
+    // plane - the invisible plane which will catch the mouse click
+    const plane = new THREE.Plane();
+    // ray caster - casts the ray between camera and the cursor
+    const rayCaster = new THREE.Raycaster();
+
+    window.addEventListener("click", (e) => {
+      // set click coord
+      mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+      planeNormal.copy(this.camera.position).normalize();
+      plane.setFromNormalAndCoplanarPoint(
+        planeNormal,
+        new THREE.Vector3(0, 0, 0),
+      );
+      rayCaster.setFromCamera(mouse, this.camera);
+      rayCaster.ray.intersectPlane(plane, intersectionPoint);
+
+      // make object
+      const sphere = new THREE.Mesh(
+        new THREE.SphereGeometry(0.1, 10, 10),
+        new THREE.MeshBasicMaterial({
+          color: 0x00ff00,
+        }),
+      );
+      this.scene.add(sphere);
+      sphere.position.copy(intersectionPoint);
+    });
   }
 
   // Animate Scene
