5cbd8081f5f6ad5320c9c41cc8eed720dab8e06a
NadmanAlvee <nadmanalvee@gmail.com>
2026-04-16 21:09:19 +0600

    immersive camera movement complete

--- chapter_16_ImmersiveExperience/src/js/index.js
+++ chapter_16_ImmersiveExperience/src/js/index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import gsap from "gsap";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
@@ -11,6 +12,7 @@ class World {
     this.scene = this.#initScene();
     this.camera = this.#initPerspectiveCamera();
 
+    this.loadingManager = this.#initLoadingManager();
     this.controls = this.#initControl();
     this.gltfLoader = this.#initGltfLoader();
     this.hdrTextureLoader = this.#hdrTextureLoader();
@@ -19,6 +21,8 @@ class World {
     this.#initBackground();
     this.#initObjects();
     this.groundMesh = null;
+
+    this.#ImmersiveViewer();
 
     this.clock = new THREE.Clock();
     this.#initAnimationLoop();
@@ -32,6 +36,8 @@ class World {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFShadowMap;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.outputColorSpace = THREE.SRGBColorSpace;
     document.body.append(renderer.domElement);
     return renderer;
   }
@@ -55,37 +61,66 @@ class World {
     return camera;
   }
 
+  #initLoadingManager() {
+    const loadingManager = new THREE.LoadingManager();
+
+    loadingManager.onStart = (url, loaded, itemsTotal) => {
+      console.log(`Started loading`);
+    };
+
+    const progressBar = document.getElementById("progress-bar");
+
+    loadingManager.onProgress = (url, loaded, itemsTotal) => {
+      console.log(`Loading ${loaded} of ${itemsTotal} resources. ${url}`);
+      progressBar.value = Math.abs((loaded / itemsTotal) * 100);
+    };
+
+    loadingManager.onError = (url) => {
+      console.log(`An error occured while loading! ${url}`);
+    };
+
+    const progressBarContainer = document.querySelector(
+      ".loading-progress-container",
+    );
+    loadingManager.onLoad = (url, loaded, itemsTotal) => {
+      console.log(`All resources have loaded.`);
+      progressBarContainer.style.display = "none";
+    };
+
+    return loadingManager;
+  }
+
   // Orbit Control
   #initControl() {
     // Orbit Control
-    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    // const controls = new OrbitControls(this.camera, this.renderer.domElement);
     // controls.minDistance = 3;
     // controls.maxDistance = 20;
     // controls.maxPolarAngle = Math.PI / 2.1;
-    controls.update();
+    // controls.update();
 
     // First Person Control
-    // const controls = new FirstPersonControls(
-    //   this.camera,
-    //   this.renderer.domElement,
-    // );
-    // controls.movementSpeed = 8;
-    // controls.lookSpeed = 0.08;
-    // controls.lookVertical = true;
-    // controls.activeLook = true;
+    const controls = new FirstPersonControls(
+      this.camera,
+      this.renderer.domElement,
+    );
+    controls.activeLook = false;
+    controls.movementSpeed = 8;
+    controls.lookSpeed = 0.08;
+    controls.lookVertical = false;
 
     return controls;
   }
 
   // Gltf Loader
   #initGltfLoader() {
-    const gltfLoader = new GLTFLoader();
+    const gltfLoader = new GLTFLoader(this.loadingManager);
     return gltfLoader;
   }
 
   // hdr loader
   #hdrTextureLoader() {
-    const hdrTextureLoader = new HDRLoader();
+    const hdrTextureLoader = new HDRLoader(this.loadingManager);
     return hdrTextureLoader;
   }
 
@@ -110,6 +145,7 @@ class World {
     // City
     // this.city = await this.gltfLoader.loadAsync("./models/city/scene.gltf");
     // this.scene.add(this.city.scene);
+
     // The Kings Hall
     this.the_king_s_hall = await this.gltfLoader.loadAsync(
       "./models/the_king_s_hall/scene.gltf",
@@ -117,10 +153,161 @@ class World {
     this.scene.add(this.the_king_s_hall.scene);
   }
 
+  #ImmersiveViewer() {
+    window.addEventListener("keydown", (e) => {
+      this.controls.enabled = false;
+      // console.log("Key: ", e.key.toLowerCase());
+
+      const moveCamera = ({ position, rotation, posDuration, rotDuration }) => {
+        gsap.to(this.camera.position, {
+          x: position?.x,
+          y: position?.y,
+          z: position?.z,
+          duration: posDuration,
+          ease: "ease",
+        });
+        gsap.to(this.camera.rotation, {
+          x: rotation?.x,
+          y: rotation?.y,
+          z: rotation?.z,
+          duration: rotDuration,
+          ease: "ease",
+        });
+      };
+
+      switch (e.key.toLowerCase()) {
+        case "1": {
+          moveCamera({
+            position: {
+              x: -0.5690165222223935,
+              y: 1.0487999999895692,
+              z: 13.27797291473903,
+            },
+            rotation: {
+              x: -0,
+              y: 1.5471916562329344,
+              z: -0,
+            },
+            posDuration: 2.6,
+            rotDuration: 3.2,
+          });
+          break;
+        }
+        case "2": {
+          moveCamera({
+            position: {
+              x: -1.9551721870680718,
+              y: 1.4743999999940396,
+              z: 5.515766060606381,
+            },
+            rotation: {
+              x: -0,
+              y: 0.039004757749663735,
+              z: -0,
+            },
+            posDuration: 2.6,
+            rotDuration: 3.2,
+          });
+          break;
+        }
+        case "3": {
+          moveCamera({
+            position: {
+              x: 2.8,
+              y: 0,
+              z: 3.6,
+            },
+            rotation: {
+              x: 0,
+              y: -2,
+              z: 0,
+            },
+            posDuration: 2.6,
+            rotDuration: 3.2,
+          });
+          break;
+        }
+        case "4": {
+          moveCamera({
+            position: {
+              x: 2.5,
+              y: -0.9,
+              z: 12.2,
+            },
+            rotation: {
+              x: 0.9,
+              y: 0.6,
+              z: -0.6,
+            },
+            posDuration: 2.6,
+            rotDuration: 3.2,
+          });
+          break;
+        }
+        case "5": {
+          moveCamera({
+            position: {
+              x: 2.7,
+              y: 0.6,
+              z: 3.7,
+            },
+            rotation: {
+              x: 0.6,
+              y: 1.9,
+              z: -0.6,
+            },
+            posDuration: 2.6,
+            rotDuration: 3.2,
+          });
+          break;
+        }
+        case "6": {
+          moveCamera({
+            position: {
+              x: -1.7,
+              y: 0,
+              z: 8.7,
+            },
+            rotation: {
+              x: 0,
+              y: 4.7,
+              z: 0,
+            },
+            posDuration: 2.6,
+            rotDuration: 3.2,
+          });
+          break;
+        }
+        case "7": {
+          moveCamera({
+            position: {
+              x: 0.5,
+              y: 0.8,
+              z: 10,
+            },
+            rotation: {
+              x: 0.3,
+              y: 1.65,
+              z: -0.3,
+            },
+            posDuration: 2.6,
+            rotDuration: 3.2,
+          });
+          break;
+        }
+        case "g": {
+          console.log("Location: ", this.camera.position.clone());
+          console.log("Rotation: ", this.camera.rotation.clone());
+          break;
+        }
+      }
+    });
+  }
+
   // Animate Scene
   #initAnimationLoop() {
     this.renderer.setAnimationLoop((time) => {
-      this.controls.update();
+      this.controls.update(this.clock.getDelta());
       this.renderer.render(this.scene, this.camera);
     });
   }
