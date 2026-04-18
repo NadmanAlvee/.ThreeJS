352b6a4530ac82cb9f793985df151116c6e57fbb
NadmanAlvee <nadmanalvee@gmail.com>
2026-04-18 11:10:22 +0600

    self driving car complete

---
+++ chapter_18_SelfDrivingCar/src/js/index.js
@@ -0,0 +1,236 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { HDRLoader } from "three/addons/loaders/HDRLoader.js";
+
+import * as YUKA from "yuka";
+
+class World {
+  // constructor
+  constructor() {
+    this.renderer = this.#initRenderer();
+    this.scene = this.#initScene();
+    this.camera = this.#initPerspectiveCamera();
+
+    this.controls = this.#initControl();
+    this.gltfLoader = this.#initGltfLoader();
+    this.hdrTextureLoader = this.#hdrTextureLoader();
+
+    this.#init();
+  }
+
+  async #init() {
+    this.#initLights();
+    this.#initBackground();
+
+    this.entityManager = this.#yukaEntityManager();
+
+    this.copVehicle = await this.#initCarModel();
+    this.vehicleMesh = null;
+    this.vehicle = null;
+    this.syncFunction = null;
+    this.#initObjects();
+
+    this.path = this.#initPath();
+    this.#displayPath();
+    this.#makeVehicleFollowPath();
+
+    this.yukaTime = new YUKA.Time();
+
+    this.#initAnimationLoop();
+    this.#initResize();
+  }
+
+  // Render
+  #initRenderer() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.shadowMap.type = THREE.PCFShadowMap;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.outputColorSpace = THREE.SRGBColorSpace;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
+    document.body.append(renderer.domElement);
+    return renderer;
+  }
+
+  // Scene
+  #initScene() {
+    const scene = new THREE.Scene();
+    return scene;
+  }
+
+  // Camera
+  #initPerspectiveCamera() {
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000,
+    );
+    camera.position.set(15, 5, 25);
+    return camera;
+  }
+
+  // Orbit Control
+  #initControl() {
+    // Orbit Control
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.update();
+
+    // First Person Control
+    // const controls = new FirstPersonControls(
+    //   this.camera,
+    //   this.renderer.domElement,
+    // );
+    // controls.activeLook = false;
+    // controls.movementSpeed = 8;
+    // controls.lookSpeed = 0.08;
+    // controls.lookVertical = false;
+
+    return controls;
+  }
+
+  // Gltf Loader
+  #initGltfLoader() {
+    const gltfLoader = new GLTFLoader();
+    return gltfLoader;
+  }
+
+  // hdr loader
+  #hdrTextureLoader() {
+    const hdrTextureLoader = new HDRLoader();
+    return hdrTextureLoader;
+  }
+
+  // Background
+  #initBackground() {
+    this.scene.background = new THREE.Color(0x979797);
+  }
+  // Lights
+  #initLights() {
+    const ambientLight = new THREE.AmbientLight(0x333333);
+    this.scene?.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight("#dddddd", 2);
+    directionalLight.position.set(10, 20, 0);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera.bottom = -12;
+    this.scene?.add(directionalLight);
+  }
+
+  #yukaEntityManager() {
+    const entityManager = new YUKA.EntityManager();
+    return entityManager;
+  }
+
+  async #initCarModel() {
+    // cop car model
+    const cop = await this.gltfLoader.loadAsync("./models/Cop.gltf", (gltf) => {
+      return gltf;
+    });
+    cop.scene.scale.set(0.5, 0.5, 0.5);
+    this.scene.add(cop.scene);
+
+    return cop;
+  }
+
+  // Initiate Objects
+  #initObjects() {
+    // entity vehicle cone
+    // const vehicleGeometry = new THREE.ConeGeometry(0.1, 0.5, 8);
+    // vehicleGeometry.rotateX(Math.PI * 0.5);
+    // this.vehicleMesh = new THREE.Mesh(
+    //   vehicleGeometry,
+    //   new THREE.MeshNormalMaterial(),
+    // );
+    // this.vehicleMesh.matrixAutoUpdate = false;
+    // this.scene.add(this.vehicleMesh);
+
+    this.copVehicle.scene.matrixAutoUpdate = false;
+
+    // yuka body and sync fucntion
+    this.syncFunction = (entity, renderComponent) => {
+      renderComponent.matrix.copy(entity.worldMatrix);
+    };
+
+    this.vehicle = new YUKA.Vehicle();
+    this.vehicle.setRenderComponent(this.copVehicle.scene, this.syncFunction);
+
+    this.entityManager.add(this.vehicle);
+  }
+
+  // yuka path
+  #initPath() {
+    const path = new YUKA.Path();
+
+    path.add(new YUKA.Vector3(-10, 0, 12));
+    path.add(new YUKA.Vector3(0, 0, 12));
+    path.add(new YUKA.Vector3(10, 0, 12));
+    path.add(new YUKA.Vector3(15, 0, 8));
+    path.add(new YUKA.Vector3(17, 0, 0));
+    path.add(new YUKA.Vector3(-15, 0, -8));
+    path.add(new YUKA.Vector3(-17, 0, 0));
+    path.add(new YUKA.Vector3(-15, 0, 8));
+
+    path.loop = true;
+
+    return path;
+  }
+
+  #displayPath() {
+    const position = [];
+
+    for (let i = 0; i < this.path._waypoints.length; i++) {
+      const waypoint = this.path._waypoints[i];
+      position.push(waypoint.x, waypoint.y, waypoint.z);
+    }
+
+    const lineGeometry = new THREE.BufferGeometry();
+    lineGeometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(position, 3),
+    );
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+    const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
+    this.scene.add(lines);
+  }
+
+  #makeVehicleFollowPath() {
+    this.vehicle.position.copy(this.path.current());
+
+    const followPath = new YUKA.FollowPathBehavior(this.path, 5);
+    this.vehicle.steering.add(followPath);
+
+    const onPathBehavior = new YUKA.OnPathBehavior(this.path);
+    onPathBehavior.radius = 1;
+    this.vehicle.steering.add(onPathBehavior);
+
+    this.vehicle.maxSpeed = 15;
+  }
+
+  // Animate Scene
+  #initAnimationLoop() {
+    this.renderer.setAnimationLoop((time) => {
+      // yuka animate
+      const deltaTime = this.yukaTime.update().getDelta();
+      this.entityManager.update(deltaTime);
+
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  // Handle Screen Resize
+  #initResize() {
+    window.addEventListener("resize", () => {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+    });
+  }
+}
+
+const myWorld = new World();
