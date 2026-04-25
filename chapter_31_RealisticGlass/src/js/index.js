07ff4a694f3494c44dbc4e9e825e8d652c5366c4
NadmanAlvee <nadmanalvee@gmail.com>
2026-04-25 14:15:22 +0600

    ch31 complete

---
+++ chapter_31_RealisticGlass/src/js/index.js
@@ -0,0 +1,142 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { HDRLoader } from "three/addons/loaders/HDRLoader.js";
+
+class World {
+  // constructor
+  constructor() {
+    this.renderer = this.#initRenderer();
+    this.scene = this.#initScene();
+    this.camera = this.#initPerspectiveCamera();
+
+    this.controls = this.#initOrbitControl();
+    this.gltfLoader = this.#initGltfLoader();
+    this.hdrTextureLoader = this.#hdrTextureLoader();
+
+    // this.#initLights();
+    this.#initBackground();
+    this.#initObjects();
+    this.groundMesh = null;
+
+    this.#initAnimationLoop();
+    this.#initResize();
+  }
+
+  // Render
+  #initRenderer() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
+    document.body.append(renderer.domElement);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    // renderer.toneMappingExposure = 0.8;
+    return renderer;
+  }
+
+  // Scene
+  #initScene() {
+    const scene = new THREE.Scene();
+    return scene;
+  }
+
+  // Camera
+  #initPerspectiveCamera() {
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000,
+    );
+    camera.position.set(0, 15, 30);
+    return camera;
+  }
+
+  // Orbit Control
+  #initOrbitControl() {
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.update();
+    return controls;
+  }
+
+  // Gltf Loader
+  #initGltfLoader() {
+    const gltfLoader = new GLTFLoader();
+    return gltfLoader;
+  }
+
+  // hdr loader
+  #hdrTextureLoader() {
+    const hdrTextureLoader = new HDRLoader();
+    return hdrTextureLoader;
+  }
+
+  // Background
+  async #initBackground() {
+    const textureSrc = ["MR_INT-003_Kitchen_Pierre.hdr"];
+    const hdrTexture = await this.hdrTextureLoader.loadAsync(
+      `./assets/${textureSrc[0]}`,
+    );
+
+    if (hdrTexture) {
+      hdrTexture.mapping = THREE.EquirectangularReflectionMapping;
+      this.scene.background = hdrTexture;
+      // this.scene.environment = hdrTexture;
+    }
+
+    // this.scene.background = new THREE.Color(0xededed);
+  }
+  // Lights
+  #initLights() {
+    const ambientLight = new THREE.AmbientLight(0x333333);
+    this.scene?.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight("#ffffff", 2);
+    directionalLight.position.set(10, 20, 0);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera.bottom = -12;
+    this.scene?.add(directionalLight);
+  }
+
+  // Initiate Objects
+  async #initObjects() {
+    // Sphere 1
+    const sphere1EnvMap = await this.hdrTextureLoader.loadAsync(
+      "./assets/MR_INT-003_Kitchen_Pierre.hdr",
+    );
+    sphere1EnvMap.mapping = THREE.EquirectangularReflectionMapping;
+    const sphereGeometry = new THREE.SphereGeometry(10);
+    const sphereMaterial = new THREE.MeshPhysicalMaterial({
+      roughness: 0,
+      metalness: 0,
+      envMap: sphere1EnvMap,
+      transmission: 1,
+      ior: 2.33,
+      color: 0x0000ff,
+    });
+    const sphere1 = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    this.scene.add(sphere1);
+  }
+
+  // Animate Scene
+  #initAnimationLoop() {
+    this.renderer.setAnimationLoop((time) => {
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  // Handle Screen Resize
+  #initResize() {
+    window.addEventListener("resize", () => {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+    });
+  }
+}
+
+const myWorld = new World();
